72c52a9cc15bcc57aa52617bf7135166a40adc42
GakutoSeita <gakuto.seita@mitsubishicorp.com>
2022-01-23 12:41:02 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>”ありがとう”の投稿</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">これまで届いた”ありがとう”を見る</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="insert.php">
+  <!--<div class="jumbotron">-->
+  <div class="">
+   <fieldset>
+    <legend>同僚への”ありがとう”を投稿しよう！</legend>
+     <label>あなたのお名前<br>
+      <input type="text" name="name"></label><br>
+     <label>”ありがとう”を伝えたい人のお名前(敬称不要)<br>
+      <input type="text" name="toname"></label><br>
+     <label>”ありがとう”のストーリーとメッセージ<br>
+      <textArea name="comment" rows="4" cols="40"></textArea></label><br>
+      <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,44 @@
+<?php
+// 1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+
+$name = $_POST['name'];
+$toname = $_POST['toname'];
+$comment = $_POST['comment'];
+
+
+
+// 2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=arigatou_db;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+
+// ３．SQL文を用意(データ登録：INSERT)
+$stmt = $pdo->prepare(
+  "INSERT INTO gs_arigatou_table(id,name,toname,comment,indate)
+  VALUES( NULL, :name, :toname, :comment, sysdate() )"
+);
+
+// 4. バインド変数を用意
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':toname', $toname, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+// 5. 実行
+$status = $stmt->execute();
+
+// 6．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("ErrorMassage:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header('Location: index.php');
+}
+?>
